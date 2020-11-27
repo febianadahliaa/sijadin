827d2479c66f5bcaec9c4316415c8b2f6bcadcbb
febianadahliaa@gmail.com <febianadahliaa@gmail.com>
2020-11-27 08:52:18 +0700

    create kepala bps page

---
+++ application/controllers/Kepala_bps.php
@@ -0,0 +1,25 @@
+<?php
+
+class Kepala_bps extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('perjadin_model');
+		//if($this->kasie_model->isNotLogin()) redirect(site_url('kasie/login'));
+	}
+
+	public function index()
+	{
+		$data['title'] = 'Daftar Perjadin Saya';
+		$data['data_perjadin'] = $this->perjadin_model->getAll();
+        
+        $this->load->view('partials_/header', $data);
+        $this->load->view('partials_/sidebar', $data);
+        $this->load->view('partials_/topbar', $data);
+        $this->load->view('kepala_bps/index', $data);
+        $this->load->view('partials_/footer', $data);
+	}
+}
+
+?>
---
+++ application/views/kepala_bps/index.php
@@ -0,0 +1,49 @@
+h<!-- Begin Page Content -->
+<div class="container-fluid">
+    <!-- </?php $this->load->view('kasie/_partials/breadcrumb.php') ?> -->
+
+    <!-- Data Tables -->					
+    <div class="card mb-3">
+        <div class="card-header">
+            <h5>DAFTAR PERJADIN SAYA</h5>	
+        </div>
+
+        <div class="card-body">
+            <div>
+                <select id="bulan" name="Bulan">
+                    <option value="januari">Januari</option>
+                    <option value="februari">Februari</option>
+                    <option value="maret">Maret</option>
+                    <option value="april">April</option>
+                    <option value="mei">Mei</option>
+                    <option value="juni">Juni</option>
+                    <option value="juli">Juli</option>
+                    <option value="agustus">Agustus</option>
+                    <option value="september">September</option>
+                    <option value="oktober">Oktober</option>
+                    <option value="november">November</option>
+                    <option value="desember">Desember</option>
+                </select>	
+            </div>
+            <br>
+            <div class="table-responsive">
+                <table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Tanggal</th>
+                            <th>Kegiatan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($data_perjadin as $nilai): ?>
+                        <tr>
+                            <td><?php echo $nilai->tanggal ?></td>
+                            <td><?php echo $nilai->namaAtribut . ' ' . $nilai->namaKegiatan ?></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div> <!-- /.table-responsive -->
+        </div> <!-- /.card-body -->
+    </div>
+</div> <!-- /.container-fluid -->
