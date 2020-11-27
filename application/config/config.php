2fcbd479beacfc70fa0462cb192dce124b9e0b1f
febianadahliaa@gmail.com <febianadahliaa@gmail.com>
2020-11-27 08:43:31 +0700

    remove index.php from the url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sijadin';
+$config['base_url'] = 'http://localhost/sijadin/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/sijadin';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = ''; //index.php
 
 /*
 |--------------------------------------------------------------------------
