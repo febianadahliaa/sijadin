a270a1dbff3995032a8762c7c1dae93b9917a22e
febianadahliaa@gmail.com <febianadahliaa@gmail.com>
2020-12-04 14:41:23 +0700

    add profile page

---
+++ application/controllers/Profile.php
@@ -0,0 +1,26 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Profile extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Matriks Perjadin Pegawai';
+        $data['perjadin'] = $this->perjadin_model->getAll();
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        
+        $this->load->view('partials_/header', $data);
+        $this->load->view('partials_/sidebar', $data);
+        $this->load->view('partials_/topbar', $data);
+        $this->load->view('profile', $data);
+        $this->load->view('partials_/footer');
+    }
+}
+
+?>
---
+++ application/views/profile.php
@@ -0,0 +1,18 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <div class="card mb-3" style="max-width: 540px;">
+    <div class="row no-gutters">
+        <!-- <div class="col-md-4">
+        <img src="..." class="card-img" alt="...">
+        </div> -->
+        <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title"><?= $user['name']; ?>/h5>
+            <p class="card-text"><?= $user['email']; ?></p>
+            <p class="card-text"><?= $user['phone']; ?></p>
+            <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+        </div>
+        </div>
+    </div>
+    </div>
+</div>  <!-- /.container-fluid -->
