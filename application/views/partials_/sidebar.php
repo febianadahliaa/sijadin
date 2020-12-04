e09e0e986e797d9ed3b045762fcb36498711dd4d
febianadahliaa@gmail.com <febianadahliaa@gmail.com>
2020-12-04 08:51:21 +0700

    edit topbar and sidebar

--- application/views/partials_/sidebar.php
+++ application/views/partials_/sidebar.php
@@ -1,76 +1,76 @@
-        <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-code"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">sijadin</div>
-            </a> <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-mail-bulk"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">sijadin</div>
+    </a> <!-- Sidebar - Brand -->
 
-            <hr class="sidebar-divider">
+    <hr class="sidebar-divider">
 
-            <div class="sidebar-heading">
-                Perjadin saya
-            </div>
-            
-            <li class="nav-item">
-                <a class="nav-link" href="<?= site_url('kasie') ?>">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Perjadin</span>
-                </a>
-            </li>
+    <div class="sidebar-heading">
+        Perjadin saya
+    </div>
+    
+    <li class="nav-item">
+        <a class="nav-link" href="<?= site_url('kasie') ?>">
+            <i class="fas fa-fw fa-clipboard-list"></i>
+            <span>Perjadin</span>
+        </a>
+    </li>
 
-            <hr class="sidebar-divider">
+    <hr class="sidebar-divider">
 
-            <div class="sidebar-heading">
-                Perjadin pegawai
-            </div>
-            
-            <!-- Nav Item -->
-            <li class="nav-item">
-                <a class="nav-link" href="<?= site_url('kasie/input_perjadin') ?>">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Input Perjadin</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= site_url('kasie/list_perjadin') ?>">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>List Perjadin</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= site_url('kasie/matriks_perjadin') ?>">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Matriks Perjadin</span>
-                </a>
-            </li>
+    <div class="sidebar-heading">
+        Perjadin pegawai
+    </div>
+    
+    <!-- Nav Item -->
+    <li class="nav-item">
+        <a class="nav-link" href="<?= site_url('kasie/input_perjadin') ?>">
+            <i class="fas fa-fw fa-pencil-alt"></i>
+            <span>Input Perjadin</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="<?= site_url('kasie/list_perjadin') ?>">
+            <i class="fas fa-fw fa-th-list"></i>
+            <span>List Perjadin</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="<?= site_url('kasie/matriks_perjadin') ?>">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Matriks Perjadin</span>
+        </a>
+    </li>
 
-            <hr class="sidebar-divider">
+    <hr class="sidebar-divider">
 
-            <div class="sidebar-heading">
-                Pengaturan
-            </div>
+    <div class="sidebar-heading">
+        Profil
+    </div>
 
-            <!-- Nav Item - 'Pengaturan' Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <i class="fas fa-fw fa-user"></i>
-                    <span>Profil saya</span>
-                </a>
-                <a class="nav-link" href="<?= base_url('auth/logout') ?>">
-                    <i class="fas fa-fw fa-sign-out-alt"></i>
-                    <span>Keluar</span>
-                </a>                
-            </li>
+    <!-- Nav Item - 'Pengaturan' Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('kasie/profile') ?>">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Profil saya</span>
+        </a>
+        <a class="nav-link" href="<?= base_url('auth/logout') ?>" data-toggle="modal" data-target="#logoutModal">
+            <i class="fas fa-fw fa-sign-out-alt"></i>
+            <span>Keluar</span>
+        </a>                
+    </li>
 
-            <hr class="sidebar-divider">
+    <hr class="sidebar-divider">
 
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
 
-        </ul>
-        <!-- End of Sidebar -->
+</ul>
+<!-- End of Sidebar -->
--- application/views/partials_/topbar.php
+++ application/views/partials_/topbar.php
@@ -1,84 +1,89 @@
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
 
-            <!-- Main Content -->
-            <div id="content">
+    <!-- Main Content -->
+    <div id="content">
 
-                <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-                    <!-- Supaya fleksibel ketika ukuran di window dikecilkan secara horizontal -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
+            <!-- Supaya fleksibel ketika ukuran di window dikecilkan secara horizontal -->
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                <i class="fa fa-bars"></i>
+            </button>
 
-                    <!-- Pencarian -->
-                    <!-- <form
-                        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-                                aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
+            <!-- Pencarian -->
+            <!-- <form
+                class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                <div class="input-group">
+                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                        aria-label="Search" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="button">
+                            <i class="fas fa-search fa-sm"></i>
+                        </button>
+                    </div>
+                </div>
+            </form> -->
+
+            <!-- Topbar Navbar -->
+            <ul class="navbar-nav ml-auto">
+
+                <!-- Tombol pencarian yang visible -->
+                <li class="nav-item dropdown no-arrow d-sm-none">
+                    <!-- <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-search fa-fw"></i>
+                    </a> -->
+                    <!-- Dropdown - Messages -->
+                    <!-- <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                        aria-labelledby="searchDropdown">
+                        <form class="form-inline mr-auto w-100 navbar-search">
+                            <div class="input-group">
+                                <input type="text" class="form-control bg-light border-0 small"
+                                    placeholder="Search for..." aria-label="Search"
+                                    aria-describedby="basic-addon2">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="button">
+                                        <i class="fas fa-search fa-sm"></i>
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </form> -->
+                        </form>
+                    </div> -->
+                </li>
 
-                    <!-- Topbar Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                <div class="topbar-divider d-none d-sm-block"></div>
 
-                        <!-- Tombol pencarian yang visible -->
-                        <li class="nav-item dropdown no-arrow d-sm-none">
-                            <!-- <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-search fa-fw"></i>
-                            </a> -->
-                            <!-- Dropdown - Messages -->
-                            <!-- <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                                aria-labelledby="searchDropdown">
-                                <form class="form-inline mr-auto w-100 navbar-search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small"
-                                            placeholder="Search for..." aria-label="Search"
-                                            aria-describedby="basic-addon2">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
-                                                <i class="fas fa-search fa-sm"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div> -->
-                        </li>
+                <!-- Nav Item - User Information -->
+                <li class="nav-item dropdown no-arrow">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $user['name']; ?></span>
+                        <img class="img-profile rounded-circle" src="<?= base_url('assets/img/profile/male.jpg') ?>">
+                        <!-- <img class="img-profile rounded-circle" src="</?=base_url('assets/img/profile/').$user['image']?>"> -->
+                    </a>
 
-                        <div class="topbar-divider d-none d-sm-block"></div>
+                    <!-- Dropdown - User Information -->
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                        aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Profil Saya
+                        </a>
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-key fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Ganti Password
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="<?= base_url('auth/logout') ?>" data-toggle="modal" data-target="#logoutModal">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Keluar
+                        </a>
+                    </div>
+                </li>
 
-                        <!-- Nav Item - User Information -->
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <!-- <span class="mr-2 d-none d-lg-inline text-gray-600 small"></?= $user['nama']; ?></span> -->
-                                <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
-                            </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profil Saya
-                                </a>
-                                <!-- <div class="dropdown-divider"></div> -->
-                                <a class="dropdown-item" href="<?= base_url('auth/logout') ?>" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Keluar
-                                </a>
-                            </div>
-                        </li>
+            </ul>
 
-                    </ul>
-
-                </nav>
-                <!-- End of Topbar -->
+        </nav>
+        <!-- End of Topbar -->
