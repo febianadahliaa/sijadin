8ae85bc6e7c042cc7d44f92954a9bbe0bba5db41
febianadahliaa@gmail.com <febianadahliaa@gmail.com>
2020-12-28 10:00:25 +0700

    make dynamic image profile

--- application/views/partials_/topbar.php
+++ application/views/partials_/topbar.php
@@ -60,8 +60,7 @@
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $user['name']; ?></span>
-                        <img class="img-profile rounded-circle" src="<?= base_url('assets/img/profile/male.jpg') ?>">
-                        <!-- <img class="img-profile rounded-circle" src="</?=base_url('assets/img/profile/').$user['image']?>"> -->
+                        <img class="img-profile rounded-circle" src="<?= ($user['gender'] == 'male') ? base_url('assets/img/profile/male.jpg') : base_url('assets/img/profile/female.jpg') ?>">
                     </a>
 
                     <!-- Dropdown - User Information -->
